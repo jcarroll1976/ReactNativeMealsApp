0a9a553ce00feaade6c095eb7ba6caf940b3a4ac
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-13 18:45:09 -0400

    Installed react navigation to start implementing navigation within app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from 'react-native';
+
 import CategoryScreen from './screens/CategoryScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='light' />
-    <CategoryScreen />
+    <StatusBar style='dark' />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </>
   );
 }
