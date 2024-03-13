8fab756fc182e1b241d008c728fd0c2845c711e5
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-13 19:49:22 -0400

    Added MealsOverviewScreen, implemented navigation between screens

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,13 @@
 import { Pressable, Text, View,StyleSheet,Platform } from "react-native";
 
-export default function CategoryGridTile({title,color}) {
+export default function CategoryGridTile({title,color,onPress}) {
     return (
         <View style = {styles.gridItem}>
             <Pressable 
                 android_ripple={{color: "#ccc"}} 
-                style = {({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}>
+                style = {({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}
+                onPress={onPress}
+            >
                 <View style = {[styles.innerContainer, {backgroundColor: color}]}>
                     <Text style = {styles.title}>{title}</Text>
                 </View>
--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -3,11 +3,19 @@ import { FlatList} from "react-native";
 import { CATEGORIES } from "../data/dummy-data"
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title = {itemData.item.title} color = {itemData.item.color} />
-}
 
-export default function CategoryScreen() {
+
+export default function CategoryScreen({navigation}) {
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview')
+        }
+    
+        return <CategoryGridTile 
+                title = {itemData.item.title} 
+                color = {itemData.item.color} 
+                onPress={pressHandler} />
+    }
     return (
         <FlatList 
             data = {CATEGORIES}
