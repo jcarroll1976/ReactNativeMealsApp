5488a4b63d593e9bed2ae4e2afd5bd0c51484b1f
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-18 19:59:31 -0400

    added default options to set styles for different screens inside app

--- App.js
+++ App.js
@@ -13,8 +13,20 @@ export default function App() {
     <>
     <StatusBar style='dark' />
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {backgroundColor: "#351401"},
+            headerTintColor: "white",
+            contentStyle: {backgroundColor: "#3f2f25"}
+        }}
+      >
+        <Stack.Screen 
+          name="MealsCategories" 
+          component={CategoryScreen}
+          options={{
+            title: "Meals Categories"
+          }}
+        />
         <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
       </Stack.Navigator>
     </NavigationContainer>
