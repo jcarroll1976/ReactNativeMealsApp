8de000396cadced224e25746ce791d1f33db444f
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-20 19:46:11 -0400

    added MealDetails component to display details for each meal

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,6 @@
 import { Image, Pressable, Text, View, StyleSheet,Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 export default function MealItem({ id, title, imageUrl,duration,complexity,affordability }) {
     const navigation = useNavigation();
@@ -22,11 +23,11 @@ export default function MealItem({ id, title, imageUrl,duration,complexity,affor
                         <Image source={{uri: imageUrl}} style = {styles.image} />
                         <Text style = {styles.title}>{title}</Text>
                     </View>
-                    <View style = {styles.details}>
-                        <Text style = {styles.detailsItem}>{duration}min</Text>
-                        <Text style = {styles.detailsItem}>{complexity.toUpperCase()}</Text>
-                        <Text style = {styles.detailsItem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetails 
+                        duration={duration} 
+                        complexity={complexity}
+                        affordability={affordability}
+                    />
                 </View>
             </Pressable>
         </View>
@@ -61,15 +62,5 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontSize: 18,
         margin: 8
-    },
-    details: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 8
-    },
-    detailsItem: {
-        marginHorizontal: 4,
-        fontSize: 12
     }
 })
