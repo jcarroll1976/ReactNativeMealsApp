8f7ca7c3f612c118b3d1ef1e9dd9d4ebeb6fcaf3
Joshua Carroll <carrolljosh1976@gmail.com>
2024-03-23 02:37:11 -0400

    finished MealDetailsScreen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,14 +1,16 @@
-import { Image, Text, View, StyleSheet } from "react-native";
+import { Image, Text, View, StyleSheet,ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 export default function MealDetailsScreen({ route }) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId)
     return (
-        <View>
+        <ScrollView style = {styles.rootContainer}>
             <Image style = {styles.image} source={{uri: selectedMeal.imageUrl}} />
             <Text style = {styles.title}>{selectedMeal.title}</Text>
             <MealDetails 
@@ -17,19 +19,22 @@ export default function MealDetailsScreen({ route }) {
                 affordability={selectedMeal.affordability}
                 textStyle={styles.detailText}
             />
-            <Text style = {styles.subtitle}>Ingredients</Text>
-            {selectedMeal.ingredients.map((ingredient) => (
-                <Text key={ingredient}>{ingredient}</Text>
-            ))}
-            <Text style = {styles.subtitle}>Steps</Text>
-            {selectedMeal.steps.map((step) => (
-                <Text key={step}>{step}</Text>
-            ))}
-        </View>
+            <View style = {styles.listOuterContainer}>
+                <View styles = {styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data = {selectedMeal.ingredients} />
+                    <Subtitle>Steps</Subtitle>
+                    <List data = {selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32
+    },
     image: {
         width: "100%",
         height: 350
@@ -44,10 +49,10 @@ const styles = StyleSheet.create({
     detailText: {
         color: "white"
     },
-    subtitle: {
-        color: "white",
-        fontSize: 18,
-        fontWeight: "bold",
-        margin: 6
+    listOuterContainer: {
+        alignItems: "center"
+    },
+    listContainer: {
+        width: "80%"
     }
 })
