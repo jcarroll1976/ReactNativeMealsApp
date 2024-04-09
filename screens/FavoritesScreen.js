ee25b91456106927e3e01dc809dcb0d843ebaa34
Joshua Carroll <carrolljosh1976@gmail.com>
2024-04-08 22:07:07 -0400

    added redux option for adding and removing favorited meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,15 @@
 import { StyleSheet,Text,View } from "react-native";
-import { useContext } from "react";
+//import { useContext } from "react";
 import MealsList from "../components/MealsList/MealsList";
-import { FavoritesContext } from "../store/context/favorites-context";
+//import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 
 export default function FavoritesScreen() {
-    const favoriteMealsContext = useContext(FavoritesContext);
+    //const favoriteMealsContext = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector(state => state.favoriteMeals.ids);
 
-    const favoriteMeals = MEALS.filter((meal) => favoriteMealsContext.ids.includes(meal.id));
+    const favoriteMeals = MEALS.filter((meal) => /*favoriteMealsContext.ids*/favoriteMealIds.includes(meal.id));
 
     if (favoriteMeals.length === 0) {
         return (
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,28 +1,36 @@
 import { useContext, useEffect } from "react";
 import { Image, Text, View, StyleSheet,ScrollView,Button } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
-import { FavoritesContext } from "../store/context/favorites-context";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
+
+//import { FavoritesContext } from "../store/context/favorites-context";
 
 export default function MealDetailsScreen({ route,navigation }) {
-   const favoriteMealsContext = useContext(FavoritesContext);
+   //const favoriteMealsContext = useContext(FavoritesContext);
+   const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+   const dispatch = useDispatch();
 
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId)
 
-    const mealIsFavorite = favoriteMealsContext.ids.includes(mealId);
+    //const mealIsFavorite = favoriteMealsContext.ids.includes(mealId);
+    const mealIsFavorite = favoriteMealIds.includes(mealId)
 
     function changeFavoriteStatusHandler(){
        if(mealIsFavorite) {
-        favoriteMealsContext.removeFavorite(mealId);
+        //favoriteMealsContext.removeFavorite(mealId);
+        dispatch(removeFavorite({id: mealId}));
        }
        else {
-        favoriteMealsContext.addFavorite(mealId);
+        //favoriteMealsContext.addFavorite(mealId);
+        dispatch(addFavorite({id: mealId}));
        }
     }
 
